98472f4734bd689a2ad7de0c54875ad8d339ee2b
knxxxn <20211308@sungshin.ac.kr>
2024-04-08 20:44:32 +0900

    join관계

---
+++ ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/Item.java
@@ -0,0 +1,15 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+
+
+}
