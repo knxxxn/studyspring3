51c63e8a247e5fd7ed2e21ee4d20aec6915cca9b
knxxxn <20211308@sungshin.ac.kr>
2024-07-15 21:25:09 +0900

    API 고급 -1
    스프링스터디3 - 2

--- jpabook/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
@@ -21,9 +22,11 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="order_id")
     private Order order;
+
     private int orderPrice; //주문가격
     private int count; //주문 수량
 
